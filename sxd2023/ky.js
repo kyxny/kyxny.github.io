3c55d6c02188bfea0aa1247c8c46947c910608fa
conbanwa <35380552+conbanwa@users.noreply.github.com>
2023-09-14 09:27:42 +0800

    Update ky.js

--- sxd2023/ky.js
+++ sxd2023/ky.js
@@ -1,9 +1,10 @@
 const ky = {
     "about":{
-        "company":"1",
+        "company":"台州卡友信息科技有限公司",
         "founder":"2",
-        "phone":139,
-        "findus":""
+        "phone":"暂无",
+        "email":"2577143781@qq.com",
+        "address":"浙江省台州市玉环市沙门镇"
     },
     "performance":"智能搭接救援订单日均300+，全国第一"
 }
